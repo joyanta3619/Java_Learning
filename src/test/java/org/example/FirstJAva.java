09e9f29fbbb714661299ca4fb01bdea557eb37a3
joyanta3619 <akashshom3619@gmail.com>
2025-07-19 16:57:16 +0600

    Initial commit

--- src/test/java/org/example/FirstJAva.java
+++ src/test/java/org/example/FirstJAva.java
@@ -3,7 +3,7 @@ package org.example;
 public class FirstJAva {
     public static void main(String[]args){
 
-        System.out.println("Hello Java");
+        System.out.println("Hello akash");
 
     }
 }
