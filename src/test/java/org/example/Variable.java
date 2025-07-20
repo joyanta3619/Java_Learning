8a66c9f68d7be280f32ad259c89f412f0f3c9658
joyanta3619 <akashshom3619@gmail.com>
2025-07-20 13:41:11 +0600

    Initial commit for second

---
+++ src/test/java/org/example/Variable.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Variable {
+    public static void main (String[]args){
+
+        int a=11;int b=22;
+        int total = a+b;
+
+        System.out.println(total);
+    }
+
+
+
+}
