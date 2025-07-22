103a16c547d938519e76d9035a05d063b6d4d088
joyanta3619 <akashshom3619@gmail.com>
2025-07-22 18:34:09 +0600

    Initial commit for second

---
+++ src/test/java/org/example/ForEachLoop.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ForEachLoop {
+    public static void main(String[]args){
+        int[] number ={1,2,3,4,5,6,7,8,9};
+        for(int num:number){
+            System.out.println("number : "+num);
+        }
+
+
+    }
+}
