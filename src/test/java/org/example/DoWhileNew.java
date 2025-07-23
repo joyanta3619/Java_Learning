de4b618bfc12417f883461c098fbcefac39f8c59
joyanta3619 <akashshom3619@gmail.com>
2025-07-24 00:40:03 +0600

    Initial commit for second time

--- src/test/java/org/example/DoWhileNew.java
+++ src/test/java/org/example/DoWhileNew.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class DoWhileNew {
+    public static void main(String[] args) {
+        int i = 100;
+        do {
+            System.out.println("This will run at least once.");
+        } while (i < 10);
+    }
 }
