abb12eb973acec8d2c3b05914474904c8e3f3ce7
joyanta3619 <akashshom3619@gmail.com>
2025-07-29 21:57:06 +0600

    Initial commit for second time

--- src/test/java/org/example/MethodCallingExp3.java
+++ src/test/java/org/example/MethodCallingExp3.java
@@ -11,6 +11,12 @@ public class MethodCallingExp3 {
 
         MethodExp obj1 = new MethodExp();    //MethodExp classs teke obj call kore value nie asci
         obj.sum(22,33);
+
+        MethodPractice obj2 = new MethodPractice();
+        obj2.sub(22,33);
+
+
+        MethodPractice.sum();  //Direct static theke main static  //MethodPractice theke
     }
 
 }
