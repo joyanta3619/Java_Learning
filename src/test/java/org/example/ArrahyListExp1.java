a22a2785920fb87b78c6b7a91fbedd167b712763
joyanta3619 <akashshom3619@gmail.com>
2025-07-30 18:29:37 +0600

    Initial commit for second

---
+++ src/test/java/org/example/ArrahyListExp1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class ArrahyListExp1 {
+//    Creating an ArrayList and Adding New Elements to It
+    public static void main(String[]args){
+        ArrayList<String> Names = new ArrayList<>();
+        Names.add("Apple");
+        Names.add("Banana");
+        Names.add("Cherry");
+
+        System.out.println("ArrayList: "+Names);
+    }
+}
